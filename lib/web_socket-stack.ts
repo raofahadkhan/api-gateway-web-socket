e3fda4fa5a292928723a3280736e7b163448b5d8
Rao Fahad <raofahad046@gmail.com>
2024-01-10 14:49:36 +0500

    created dynamodb table

--- lib/web_socket-stack.ts
+++ lib/web_socket-stack.ts
@@ -1,11 +1,26 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class WebSocketStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const { service, stage } = props?.tags!;
+
+    // ===============================================================================
+    // DYNAMODB: CREATED DYNAMODB TABLE FOR USERS
+    // ===============================================================================
+
+    const table = new dynamodb.Table(this, `${service}-${stage}-table`, {
+      tableName: `${service}-${stage}-table`,
+      partitionKey: {
+        name: "user_id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+    });
   }
 }
